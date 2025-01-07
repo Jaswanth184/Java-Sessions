ebdc57522d6f5964bd777ee196a064dff6057597
Jaswanth184 <jaswanthmandadapu18@gmail.com>
2025-01-07 10:49:44 +0530

    firstcommit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x =5;
+        int y =7;
+
+        if(x>y){
+            System.out.println(x:"X is greater");
+            )else{
+                System.out.println(x:"Y is greater");
+            }
+        }
+        
+            }
+        }
